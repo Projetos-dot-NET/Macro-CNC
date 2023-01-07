33a59f855536e24eb84a1843267bfe71fc738257
lobosoft2020 <lobodrigues@gmail.com>
2023-01-07 18:20:37 -0300

    trnformai em decimal antes de enviar

--- Projetos/Contorno Retangular/Form2.cs
+++ Projetos/Contorno Retangular/Form2.cs
@@ -147,7 +147,7 @@ namespace ContornoRetangular
             text_y.Text = text_y.Text.Replace(".", ",");
             text_final_z.Text = text_final_z.Text.Replace(".", ",");
             text_aprox_z.Text = text_aprox_z.Text.Replace(".", ",");
-            text_Z_troca.Text = text_Z_troca.Text.Replace(".", ",");
+            
 
             //fazer o delocamento em X, Y e Z em segurança para a localização do bloco a usinar
             if (opc_mais.Checked == true && opc_externo.Checked == true)
@@ -322,7 +322,9 @@ namespace ContornoRetangular
         }
 
         private void btn_Cab_Click(object sender, EventArgs e) //Rotina para escrever o Cabeçalho para Mach3
-        {            
+        {
+            text_Z_troca.Text = text_Z_troca.Text.Replace(".", ",");
+            
             z = Convert.ToDecimal(text_Z_troca.Text);
             text_codigo.Text = Comandos.InserirCabecalho(Round(z, 4));
         }
